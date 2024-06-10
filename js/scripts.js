d7c7547c309d6ebbc981e096d3dcb5552e9f466d
ant-ubi <antoinetteubhee@hotmail.co.uk>
2024-06-10 17:36:55 +0100

    added responsive menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hi, welcome to my site!")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
